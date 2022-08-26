69b5051c95c0407ea010a638da4a0b525a0e423b
Viktoriia Malysh <pylypenko.viktoriia@gmail.com>
2022-08-26 10:42:29 +0200

    add fix to task 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,8 +2,10 @@ const inputRangeEl = document.querySelector("#font-size-control");
 
 const spanEl = document.querySelector("#text");
 
+spanEl.style.fontSize = `${inputRangeEl.value}px`;
+
 inputRangeEl.addEventListener('input', changeStyle);
 
 function changeStyle(event) {
-    spanEl.style.fontSize = event.currentTarget.value + "px";
+    spanEl.style.fontSize= event.currentTarget.value  + "px";
 };
