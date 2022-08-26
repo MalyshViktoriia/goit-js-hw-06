df9f668d44fb78646fb3e6f7abcaec4ff48fa78a
Viktoriia Malysh <pylypenko.viktoriia@gmail.com>
2022-08-26 10:53:31 +0200

    add fix to task 9

--- js/task-09.js
+++ js/task-09.js
@@ -2,15 +2,26 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const refs = {
-  bodyEl: document.querySelector('body'),
-  magicBtn: document.querySelector('.change-color'),
-  spanOutput: document.querySelector('.color'),
-};
+// const refs = {
+//   bodyEl: document.querySelector('body'),
+//   magicBtn: document.querySelector('.change-color'),
+//   spanOutput: document.querySelector('.color'),
+// };
 
-refs.magicBtn.addEventListener('click', bodyColorHandler);
+// refs.magicBtn.addEventListener('click', bodyColorHandler);
 
-function bodyColorHandler() {
-  refs.bodyEl.style.backgroundColor = getRandomHexColor();
-  refs.spanOutput.textContent = getRandomHexColor();
-};
+// function bodyColorHandler() {
+//   refs.bodyEl.style.backgroundColor = getRandomHexColor();
+//   refs.spanOutput.textContent = getRandomHexColor();
+// };
+
+
+const buttonRef = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+buttonRef.addEventListener('click', () => {
+  spanEl.textContent = getRandomHexColor();
+  document.body.setAttribute(
+    'style',
+    'background-color: ' + spanEl.textContent
+  );
+});
